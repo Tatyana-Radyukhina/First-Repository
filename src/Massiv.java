f4a6f493fa67494e201cc35dc686b6e66fe2117a
Tatyana Radyukhina <t.a.radyukhina@yandex.ru>
2021-04-16 17:14:35 +0300

    commits

--- src/Massiv.java
+++ src/Massiv.java
@@ -4,6 +4,7 @@ public class Massiv {
     public static void main(String[] args) {
         String[] str = {"a","b","c","d","e"};
         int[] massiv = {1,2,3,4,5};
+        int[] massiv1 = {5,6,7,6,8};
         String[] result = new String[5];
         for (int i = 0; i < str.length; i++){
             result[i] = str[i] + massiv[i];
