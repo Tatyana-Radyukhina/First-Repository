ae12c52fcc5e3ee5fce4c8be17a4af120d7b13ea
Tatyana Radyukhina <t.a.radyukhina@yandex.ru>
2021-04-16 17:34:09 +0300

    commit for repository

--- src/Massiv.java
+++ src/Massiv.java
@@ -12,6 +12,8 @@ public class Massiv {
         }
         System.out.println(Arrays.toString(result));
         System.out.println("Hello World!");
+        System.out.println("Hello World - 2");
+        System.out.println("Hello World - 3");
     }
 
 }
